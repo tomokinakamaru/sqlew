d23fe3e1a1675b3f34dbd8571b2f6eab0dd3df71
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2015-01-09 12:27:26 +0900

    changed sqlew.Client.connection, cursor to methods

--- sqlew/client.py
+++ sqlew/client.py
@@ -26,17 +26,50 @@ class Client(object):
         self._autoclose = False
         return self
 
-    @property
     def connection(self):
         if self._connection is None:
             self._connection = self._dbapi.connect(**self._config)
         return self._connection
 
-    @property
     def cursor(self):
         if self._cursor is None:
-            self._cursor = self.connection.cursor()
+            self._cursor = self.connection().cursor()
         return self._cursor
+
+    def commit(self):
+        if not self.nocommit:
+            self.connection().commit()
+        return self
+
+    def rollback(self):
+        self.connection().rollback()
+        return self
+
+    def close(self):
+        if self._cursor is not None:
+            self._cursor.close()
+            self._cursor = None
+
+        if self._connection is not None:
+            self._connection.close()
+            self._connection = None
+
+        return self
+
+    def exe(self, fmt, **kwargs):
+        self.cursor().execute(qformat(fmt, **kwargs))
+        self._result_cols = [c[0] for c in self.cursor().description]
+        self._result_rows = [r for r in self.cursor()]
+        self._lastrowid = self.cursor().lastrowid
+        self._rowcount = self.cursor().rowcount
+
+        if self._autoclose:
+            self.close()
+
+        else:
+            self._autoclose = True
+
+        return self
 
     def scalar(self):
         if len(self._result_rows) > 0:
@@ -60,38 +93,3 @@ class Client(object):
 
     def all(self):
         return RL([dict(zip(self._result_cols, r)) for r in self._result_rows])
-
-    def commit(self):
-        if not self.nocommit:
-            self.connection.commit()
-        return self
-
-    def rollback(self):
-        self.connection.rollback()
-        return self
-
-    def close(self):
-        if self._cursor is not None:
-            self._cursor.close()
-            self._cursor = None
-
-        if self._connection is not None:
-            self._connection.close()
-            self._connection = None
-
-        return self
-
-    def exe(self, fmt, **kwargs):
-        self.cursor.execute(qformat(fmt, **kwargs))
-        self._result_cols = [c[0] for c in self.cursor.description]
-        self._result_rows = [r for r in self.cursor]
-        self._lastrowid = self.cursor.lastrowid
-        self._rowcount = self.cursor.rowcount
-
-        if self._autoclose:
-            self.close()
-
-        else:
-            self._autoclose = True
-
-        return self
