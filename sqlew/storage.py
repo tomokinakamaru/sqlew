276e237b34e6cc4fde75cb7bf5cfb0fcea0922ff
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2015-01-09 13:33:04 +0900

    added sqlew.Storage

---
+++ sqlew/storage.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+import os
+
+
+class Storage(dict):
+    def load(self, path):
+        for f in self._load(path):
+            with open(f, 'r') as fp:
+                key = f[len(path):].lstrip('/').rstrip('.sql')
+                self[key] = self._make_compact(fp.read())
+
+    def _load(self, path):
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                if f.endswith('.sql'):
+                    yield os.path.join(root, f)
+
+    def _make_compact(self, q):
+        all_lines = q.splitlines()
+        query_lines = [l for l in all_lines if not l.startswith(('#', '--'))]
+        query = ' '.join(query_lines)
+        return ' '.join(query.split())
